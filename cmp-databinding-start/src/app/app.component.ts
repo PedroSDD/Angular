4fa3ccef4e1c110a8a530ca7cddc2bef81cf4e0d
PedroSDD <psdd91@gmail.com>
2019-01-07 10:19:31 +0000

    Project  ngIf adding event binding

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeauture = 'recipe';
+
+  onNavigate(feauture: string){
+    this.loadedFeauture = feauture;
+  }
 }
